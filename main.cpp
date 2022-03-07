87e9911f974e2c807a00315aa7cbbb6f9045b7a0
Pasti29 <karolpastewski29@gmail.com>
2022-03-07 15:23:40 +0100

    Working threads

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <ncurses.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <list>
+#include <vector>
+#include <cstdlib>
+#include <time.h>
+
+pthread_t threadDraw;
+
+std::list<std::vector<int>> L;
 
 void horizontalWall(int startX, int startY, int width, char c[2], short index)
 {
@@ -83,46 +91,75 @@ void buildTrack2(int width, int height)
     wallTrack2(startX, startY, width, height, (char*)"#", 2);
 }
 
-void* moveCar(void* threadMove)
+void *moveChar(void *args)
 {
-    pthread_t thread = (pthread_t) threadMove;
+    while (true)
+    {
+        if (!L.empty())
+        {
+            std::vector<int> v = L.front();
+            L.pop_front();
+            mvaddch(v[0], v[1], ' ');
+            mvaddch(v[2], v[3], 'C');
+            refresh();
+        }
+        // else
+        // {
+        //     sleep(1);
+        // }
+        
+    }
+    return NULL;
+}
+
+void *moveCar(void *threadMove)
+{
+    int *sleepTime = (int *)threadMove;
     int startX = 12;
     int startY = 20;
+    int newX = startX;
+    int newY = startY;
     while(true)
     {
-        
-        sleep(1);
+        L.push_back({startX, startY, newX, newY});
+        sleep(*sleepTime);
+        startX = newX;
+        startY = newY;
+        newY++;
     }
 }
 
-void moveChar(void *args)
-{
-    int *input = (int *) args; 
-    int startX = *input;
-    int startY = *(input + 1);
-    int endX = *(input + 2);
-    int endY = *(input + 3);
-
-    mvaddch(startX, startY, ' ');
-    mvaddch(endX, endY, 'C');
-    refresh();
-}
 
 int main(int argc, char const *argv[])
 {
-    pthread_t thread1, thread2;
+    srand(time(NULL));
+
+    pthread_t thread;
+    pthread_t thread1, thread2, thread3, thread4;
+    int v1, v2, v3, v4;
     initscr();
     curs_set(0);
     start_color();
     buildTrack1(100, 30);
     buildTrack2(50, 50);
     refresh();
-    sleep(2);
-    pthread_create(&thread1, NULL, moveCar, NULL);
+    sleep(5);
+    v1 = 4;
+    v2 = 3;
+    v3 = 2;
+    v4 = 1;
+    pthread_create(&thread, NULL, moveChar, NULL);
+    pthread_create(&thread1, NULL, moveCar, &v1);
     // moveCar();
-    sleep(4);
-    pthread_create(&thread2, NULL, moveCar, NULL);
+    sleep(10);
+    pthread_create(&thread2, NULL, moveCar, &v2);
+    sleep(10);
+    pthread_create(&thread3, NULL, moveCar, &v3);
+    sleep(10);
+    pthread_create(&thread4, NULL, moveCar, &v4);
+    pthread_join(thread, NULL);
     getch();
     endwin();
+
     return 0;
 }
